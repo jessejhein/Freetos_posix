8c66cf5d24862c94b6ff07427d72ab1574389776
Cheng Chung Yan <yan@amonics.com>
2005-03-22 03:45:56 +0000

    beginning from ARMSYS 44B0X uCinux #9 Since cvs add err. in too many argument

---
+++ uClinux/user/perl/x2p/util.h
@@ -0,0 +1,43 @@
+/* $RCSfile$$Revision$$Date$
+ *
+ *    Copyright (c) 1991-2001, Larry Wall
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ * $Log$
+ * Revision 1.1  2005/03/22 03:45:04  yan
+ * beginning from ARMSYS 44B0X uCinux #9
+ * Since cvs add err. in too many argument
+ *
+ */
+
+/* is the string for makedir a directory name or a filename? */
+
+#define fatal Myfatal
+
+#define MD_DIR 0
+#define MD_FILE 1
+
+#ifdef SETUIDGID
+    int		eaccess();
+#endif
+
+char	*getwd();
+int	makedir();
+
+char * cpy2 ( char *to, char *from, int delim );
+char * cpytill ( char *to, char *from, int delim );
+void growstr ( char **strptr, int *curlen, int newlen );
+char * instr ( char *big, char *little );
+char * safecpy ( char *to, char *from, int len );
+char * savestr ( char *str );
+void croak ( char *pat, ... );
+void fatal ( char *pat, ... );
+void warn  ( char *pat, ... );
+int prewalk ( int numit, int level, int node, int *numericptr );
+
+Malloc_t safemalloc (MEM_SIZE nbytes);
+Malloc_t safecalloc (MEM_SIZE elements, MEM_SIZE size);
+Malloc_t saferealloc (Malloc_t where, MEM_SIZE nbytes);
+Free_t   safefree (Malloc_t where);
