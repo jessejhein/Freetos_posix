1e9b85cd1da05f0b1e6e7591a710abc5da6e3553
Cheng Chung Yan <yan@amonics.com>
2005-06-14 02:38:52 +0000

    combine buzzer and LED control to I/O ctrl

--- mwlike8/config_mwlike8.h
+++ mwlike8/config_mwlike8.h
@@ -7,11 +7,11 @@
 // events, MUST >=2 and <16 ==============================================
 #define NR_EVENT		2
 
-// LED module ============================================================
-#define	LED_MOD			1
-#if (LED_MOD>0)
-	#define	LED_NUM		3
+// I/O ctrl module, such as LED(s), Buzzer(s) ... ========================
+#define	IO_MOD			1
+#if (IO_MOD>0)
+	#define	IO_NUM		2
 #endif
 
-                        //      LCD display
-#define GUI_LCD         0
+// LCD display ===========================================================
+#define LCD_MOD         1
