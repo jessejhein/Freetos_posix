839068687a761191d4c66444ca6c23b6770113cb
Cheng Chung Yan <yan@amonics.com>
2005-06-26 17:18:11 +0000

    create this for avr boot

--- linlike8/arch/avr/boot/boot.c
+++ linlike8/arch/avr/boot/boot.c
@@ -1,17 +1,18 @@
 /*
- *	linlike8/arch/arv/boot/boot.c
+ * linlike8/arch/arv/boot/boot.c
+ * 24th June 2005	yan	create this following linux
  *
- *	24th June 2005	yan	create this following linux
+ * When Power ON, and system init. from compiler, then come here
  *
- *	hardware driver in cpu dependent
  */
+
+extern void start_kernel(void);
 
 void main()
 {
 // cpu hardware init
 
 // goto init kernel
-	asm("ljmp _start_kernel");//start_kernel();					
-	//for (;;);									// main should never return here, but just in case, we know what happens.
+//	need to improve to reduce stack usage, replaced by ljmp
+	start_kernel();					
 }
-
