b9b39f4962a8620ab5240d77cb9c213ae13ce449
Cheng Chung Yan <yan@amonics.com>
2005-06-26 17:19:22 +0000

    create this for init. avr arch

---
+++ linlike8/arch/avr/kernel/setup.c
@@ -0,0 +1,12 @@
+/*
+ * arch/cypress_psoc/kernel/setup.c
+ *
+ * 24th June 2005	yan	create this following linux, init. this arch 
+ *
+ * init. the cpu core, and all mcu peripheral
+ */
+
+void setup_arch(void)
+{
+}
+
---
+++ linlike8/include/asm-avr/system.h
@@ -0,0 +1,11 @@
+/*
+ *	linlike8/include/asm-avr/system.h
+ *
+ *	24th June 2005	yan	create this following linux
+ *
+ */
+
+#include <avr/interrupt.h>
+
+#define sti() sei()
+
