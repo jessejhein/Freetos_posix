63f740832552c8320fb0f052ecfa11f3d2fa703e
Cheng Chung Yan <yan@amonics.com>
2005-06-27 11:44:02 +0000

    io driver

---
+++ linlike8/arch/avr/drivers/io.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 24-05-2004 cheng chung yan yan@amonics.com> for 8-bit system
+ *
+ * this module is io in avr, driver specification. So each arch. has different codes here
+ * 
+ */
+
+// including, get data linking from others ===============================================
+
+void io_open(void)
+{
+}
+
+void io_softirq(void)
+{
+}
