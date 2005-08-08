6c8e344022a6f52f562ad20a1eb1300af58a8ff5
Cheng Chung Yan <yan@amonics.com>
2005-08-08 17:07:50 +0000

    header directory and remove floating point warning

--- ucliblike8/libc/stdio/printf.c
+++ ucliblike8/libc/stdio/printf.c
@@ -19,56 +19,10 @@
 *	transfer info. to string and place into the buffer which pointed by a char pointer
 *	support reentrant
 *
-*  Testing Procedures
-*	before you compile, you must copy whole directory of ucliblike8/ to your local home
-*	compile this file in linux gcc environment : "make printf.o" in ucliblike8/
-*	testing by : "main_compile_dir/printf.o" in ucliblike8/
-*
-*  Features
-*	printf format specific
-*		- as left justify
-*		0 as pad zero / empty
-*		number as width for display
-*	print integer & floating point with
-*		left justify
-*		pad zero / empty
-*		width
-*		negative num
-*		base 10 only
-*	floating point
-*		range: -99999.0 to 99999.0
-*		Maximum decimal digit range: 5 decimal digit
-*		Maximum width range=-9 to 9
-*		must entry the width no. of decimal point & the width no.of integer
-*
-*  Examples
-*	print_integer
-*		display negative number with zero for empty position and 4 digital width
-*			unsigned char vec_buf[6];
-*			print_integer(vec_buf, "04", -45);
-*			printf("%s\n",vec_buf);
-*		display number with left justify and 5 digital width
-*			unsigned char vec_buf[6];
-*			print_integer(vec_buf, "-5", 23);
-*			printf("%s\n",vec_buf);
-*	print_float
-*		display negative number with zero for empty position and 5 digital width, 3 decimal points
-*			unsigned char vec_buf[13];
-*			print_float(vec_buf, "05.3", -13.5);
-*			printf("%s\n",vec_buf);
-*		display number with left justify and 5 digital width, 3 decimal points
-*			unsigned char vec_buf[13];
-*			print_float(vec_buf, "-5.2", 23.3);
-*			printf("%s\n",vec_buf);
-*
-*  Pay Attention
-*	MUST input a valid format flag, no chk in coding
-*	width data must great than the real width of number
-*	MUST provide enough buffer (char* out) to hold the char. data, eg. 3456, must provide 5 bytes including '\0'
 *
 */
 
-#include "printf.h"
+#include <ucliblike8/include/printf.h>
 
 struct format_flag {
 	unsigned	pad_left :1 ;							// 0 - right justify, 1 - left justify
@@ -140,7 +94,10 @@ void print_i(char* out, struct format_flag flag, int i)
 
 	*(out + flag.width--) = '\0';							// place a NULL at the end of string
 
-next:	if (i<0) {									// -ve number
+#if (FLOAT==1)
+next:
+#endif
+		if (i<0) {									// -ve number
 		i = -i;
 		flag.neg = 1;
 	}
