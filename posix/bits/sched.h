1d4cecabfbe5feb1f03671189df7d26848a9429d
Dennis Tsang <dennis@amonics.com>
2007-08-21 07:02:24 +0000

    constants for schedule policies

---
+++ posix/bits/sched.h
@@ -0,0 +1,13 @@
+#ifndef _SCHED_H
+# error "Never include <bits/sched.h> directly; use <sched.h> instead."
+#endif
+
+
+/* Scheduling algorithms.  */
+//#define SCHED_OTHER     0
+//#define SCHED_FIFO      1
+//#define SCHED_RR        2
+#define SCHED_FREERTOS   3
+#define SCHED_COROUTINE  4
+
+
---
+++ posix/sched.h
@@ -0,0 +1,7 @@
+#ifndef _SCHED_H
+#define _SCHED_H        1
+
+/* Get system specific constant and data structure definitions.  */
+#include <bits/sched.h>
+
+#endif /* sched.h  */
