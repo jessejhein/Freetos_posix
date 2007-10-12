2c6e37f2ecc646986e0a2e2ac43f4fa84fa6775a
Dennis Tsang <dennis@amonics.com>
2007-10-12 05:50:40 +0000

    added tools to autoscale a floating pt number with five characters

---
+++ demo_posix/common/tools/autoscale.c
@@ -0,0 +1,31 @@
+/*
+ * autoscale.c
+ */
+
+#include <stdio.h>
+
+/*
+ * Function:
+ * 
+ * 
+ * Output:      >0 length of string
+ *              -1 overflow, let user to decide what to print
+ *              -2 underrun, let user to decide what to print
+ *              
+ * 
+ * 
+ */
+int autoscale(char* buf, float value, char* unit){
+    int len = 0;
+    if(value > 99999.4)         len = sprintf(buf, "++.++%s", unit);
+    else if(value > 999.94)     len = sprintf(buf, "%5.0f%s", value, unit);     //XXXXX
+    else if(value > 99.994)     len = sprintf(buf, "%5.1f%s", value, unit);     //XXX.X
+    else if(value > 9.9994)     len = sprintf(buf, "%5.2f%s", value, unit);     //XX.XX
+    else if(value >= 0)         len = sprintf(buf, "%5.3f%s", value, unit);     //X.XXX
+    else if(value > -0.005)     len = sprintf(buf, "%s%s", "0.000", unit);      //0.000
+    else if(value > -9.995)     len = sprintf(buf, "%+5.2f%s", value, unit);    //-X.XX
+    else if(value > -99.95)     len = sprintf(buf, "%+5.1f%s", value, unit);    //-XX.X
+    else if(value > -9999.5)    len = sprintf(buf, "%+5.0f%s", value, unit);    //-XXXX
+    else                        len = sprintf(buf, "--.--%s", unit);             //--
+    return len;
+}
