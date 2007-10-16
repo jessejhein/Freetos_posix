8c3e728218c2c70b85f9d5ec4ef41576299db163
Dennis Tsang <dennis@amonics.com>
2007-10-16 09:34:12 +0000

    update on module examples following changes in define.h (untested)

--- demo_posix/dspic/dspic30/module/01_led/include/define.h
+++ demo_posix/dspic/dspic30/module/01_led/include/define.h
@@ -7,12 +7,13 @@
 #ifndef DEFINE_H_
 #define DEFINE_H_
 
-// Platform header file**********************************************************
-#include <p30f5011.h>
 
 /********************************************************************************
  * Configurations for FreeRTOS and its POSIX
  ********************************************************************************/
+
+// Platform header file**********************************************************
+#include <p30f5011.h>
 
 // Define User Idle Task Here **************************************************
 #define idle_process                UserIdleTask
@@ -22,6 +23,11 @@
 #if(FREERTOS_SCHE_ENABLE == 1)
 #   define start_process()          while(1){
 #   define end_process()            }
+    //==========Enable Coroutine Thread Scheduler in FREERTOS Scheduler========
+    #define CRTHREAD_ENABLE             1
+    #if(CRTHREAD_ENABLE == 1)
+    #define MAX_CRTHREAD                10
+    #endif
 #else
 #   include <coroutine_st.h>
 #   define start_process()          scrBegin
@@ -60,13 +66,10 @@
 #define MAX_UART_TX_BUF             30
 #endif //UART_MOD
 
-// I2C Module *******************************************************************
+// I2C Module ******************************************************************
 #define I2C_MOD                     0
 #if (I2C_MOD>0)
-//  ============================Device Handler=================================== 
-#define BASE_I2C                    2           // base i2c no.
-#define NO_OF_I2C                   2           // number of i2c devices
-//  ============================Device Config==================================== 
+//  ==========================I2C Speed control================================== 
 //  I2C: Baudrate options for I2C
 //   +-- I2CBRG = (Fcy/Fscl - Fcy/1111111)-1
 //   +-- Fcy = 30MHz, Fscl = 100kHz, 400kHz, or 1MHz
@@ -76,13 +79,18 @@
                                         - ( (unsigned long)SYSTEM_CLK_HZ/1111111) \
                                         - 1 \
                                      )
+#endif //I2C_MOD
 
 // I2C DAC Module ***************************************************************
-#define I2C_DAC_MOD                  0
+#define I2C_DAC_MOD                 0
+#if (I2C_DAC_MOD>0 & I2C_MOD==0)
+    #error "I2C_MOD is off. Please set I2C_MOD = 1 in <define.h>"
+#endif
 #if (I2C_DAC_MOD>0)
-//  ============================Device Handler=================================== 
+//  ============================Devices Handler================================== 
+#define BASE_I2C_DAC                2           // base i2c dac no.
 #define I2C_DAC                      "2"        // i2c device id for DAC
-//  ============================Device Config==================================== 
+//  ======================I2C control for DAC==================================== 
 //  [Factory Preset] [A1] [A0] [R/^W]
 //     1 0 0 1 1       1    1     0        => 0x9E
 #define I2C_DAC_ADDR                0x9E        // DAC address for Writing
@@ -94,22 +102,51 @@
 #define DAC_CHC                     0x14
 #define DAC_CHD                     0x1E
 #endif //I2C_DAC_MOD
-#endif //I2C_MOD
 
-// EEPROM Module ****************************************************************
-#define EEPROM_MOD                  0           // eeprom enable
-#if (EEPROM_MOD>0)
+// Non-volatile Memory *********************************************************
+#define NVM_MOD                     0
+#if (NVM_MOD>0)
 //  ============================Device Handler=================================== 
-#define BASE_EEPROM                 3           // base eeprom no.
-#define EEPROM                      "3"         // eeprom device id
+#define BASE_EEPROM                 3           // base EEPROM no.
+#define EEPROM                      "3"         // device id for EEPROM
 //  ============================Device Config==================================== 
+    #define NVM_SRC_ON_CHIP         0
+    #define NVM_SRC_FLASH           (NVM_SRC_ON_CHIP+1)
+    #define NVM_SRC_I2C             (NVM_SRC_FLASH+1)    
+#define NVM_SRC                      NVM_SRC_ON_CHIP   //Select which EEPROM is used
+//  ==========================I2C EEPROM Config==================================
+// [Factory Preset] [A2] [A1] [A0] [R/W#]
+//    1 0 1 0         0   0    0        => 0xA0
+#if(NVM_SRC==NVM_SRC_I2C)
+#if (I2C_DAC_MOD>0 & I2C_MOD==0)
+    #error "I2C_MOD is off. Please set I2C_MOD = 1 in <define.h>"
+#endif
+#define I2C_EEPROM_ADDR             0xA0        // EEPROM address for Writing
+#define I2C_EEPROM_SIZE             0x8000      // EEPROM Size (in byte)
+#define I2C_EEPROM_PAGE_SIZE        64          // EEPROM Page size for writing (in byte)
+#endif //NVM_SRC_I2C
+//  =========================Flash EEPROM Config================================= 
+#if(NVM_SRC==NVM_SRC_FLASH)
+#define FLASH_EEPROM_BYTE_PER_WORD  2
+#define FLASH_EEPROM_WORD_PER_ROW   32
+#define FLASH_EEPROM_ROW_PER_PAGE   1
+#define FLASH_EEPROM_SIZE           (FLASH_EEPROM_BYTE_PER_WORD* \
+                                      FLASH_EEPROM_WORD_PER_ROW* \
+                                      FLASH_EEPROM_ROW_PER_PAGE)
+#endif //NVM_SRC_FLASH
+//  =======================On-chip EEPROM Config================================= 
+#if(NVM_SRC==NVM_SRC_ON_CHIP)
+#ifndef MPLAB_DSPIC30_PORT
+    #error "Target chip has no on-chip EEPROM"
+#endif
 #define EEPROM_SIZE                 1024        // EEPROM Size (in byte)
-#endif //EEPROM_MOD
+#endif //NVM_SRC_ON_CHIP
+#endif //NVM_MOD
 
-// ADC Module *******************************************************************
+// ADC Module ***************************************************************
 #define ADC_MOD                     0           // adc enable
 #if (ADC_MOD>0)
-//  ============================Device Handler=================================== 
+//  ============================Devices Handler================================== 
 #define BASE_ADC                    4           // base adc no.
 #define ADC                         "4"         // adc device id
 //  ============================Device Config==================================== 
@@ -135,6 +172,64 @@
 #define BASE_PWM                    5           // base pwm no.
 #define PWM                         "5"         // pwm device id
 #endif //PWM_MOD
+
+// Keyboard Module ***************************************************************
+#define KB_MOD                      0           // keyboard enable
+#if (KB_MOD>0)
+//  ============================Device Handler=================================== 
+#define BASE_KB                     6           // base keyboard no.
+#define KB                          "6"         // keyboard device id
+//  ============================Device Config==================================== 
+#define MAX_GPIO_BUF                10          // max number of keys stored
+//  ============================Key Map for PUSH KEY============================= 
+#define BASE_PUSH_KEY                   0       // starting key id for push keys
+#define TOTAL_PUSH_KEY                  1       // Total number of push keys
+#define PUSH_KEY0                       BASE_PUSH_KEY
+    #define EN_PKEY0                        //_TRISG14
+    #define IO_PKEY0                        //_RG14
+#define PUSH_KEY1                       PUSH_KEY0 + 1
+//  ============================Key Map for ROTARY KEY===========================
+#define BASE_ROTARY_KEY                 100     // starting key id for rotary keys
+#define TOTAL_ROTARY_KEY                1       // Total number of rotary key, each key has 2 id (up and down)
+#define ROTARY_KEY0_UP                  BASE_ROTARY_KEY
+    #define EN_RKEY0_UP                     //_TRISG6
+    #define IO_RKEY0_UP                     //_RG6
+    #define CN_RKEY0_UP                     //_CN8IE
+#define ROTARY_KEY0_DN                  ROTARY_KEY0_UP + 1
+    #define EN_RKEY0_DN                     //_TRISG7
+    #define IO_RKEY0_DN                     //_RG7
+    #define CN_RKEY0_DN                     //_CN9IE
+#define ROTARY_KEY1_UP                  ROTARY_KEY0_DN + 1
+#define ROTARY_KEY1_DN                  ROTARY_KEY1_UP + 1
+//  ============================MARCOS===========================================
+//  KEYCONFIG()
+//  Initialise the key here, EN = 1 (pin is input), CN = 1 (enable change notification)
+#define KEYCONFIG()                 do{\
+                                        EN_PKEY0 = 1; Nop(); \
+                                        \
+                                        EN_RKEY0_UP = 1; CN_RKEY0_UP = 1; \
+                                        EN_RKEY0_DN = 1; CN_RKEY0_DN = 1; \
+                                        _CNIF = 0; \
+                                        _CNIE = 1; \
+                                     }while(0)
+//  KEY_PRESS(id, ans)                                     
+//  id is the id in the key map, 
+//  ans = 1 if the key has pressed
+#define KEY_PRESS(id, ans)          do{\
+                                         switch(id){\
+                                             case PUSH_KEY0: ans = (IO_PKEY0 == 0)? 1 : 0;  break;\
+                                         }\
+                                     }while(0)
+//  RKEY_STATE(id, ans)                                    
+//  id is the id in the key map, 
+//  ans = 1 if the key at high state
+#define RKEY_STATE(id, ans)         do{\
+                                         switch(id){\
+                                            case ROTARY_KEY0_UP: ans = (IO_RKEY0_UP == 1)? 1 : 0; break; \
+                                            case ROTARY_KEY0_DN: ans = (IO_RKEY0_DN == 1)? 1 : 0; break; \
+                                         }\
+                                     }while(0)                                         
+#endif //KB_MOD
 
 // Pin usage setting ************************************************************
 #define ERR_LED0_EN()               (TRISC &= 0x7FFF)   //_RC15
