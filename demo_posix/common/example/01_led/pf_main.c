6c1c932a50b47992e25042ec3f1761ac5ed5003b
Dennis Tsang <dennis@amonics.com>
2008-01-23 08:18:15 +0000

    ==CHANGE== *restructuring examples

--- demo_posix/common/example/01_led/pf_main.c
+++ demo_posix/common/example/01_led/pf_main.c
@@ -5,7 +5,7 @@
  * DESCRIPTION:
  * This program creates 2 threads (tasks). 
  * 1)	Thread 1 flashes an led every 2 sec.
- * 2)	Thread 2 flashes an led every 6 sec.
+ * 2)	Thread 2 flashes an led every 4 sec.
  ***********************************************************************************************/
 
 #include <pthread.h>
@@ -32,10 +32,10 @@ void vUserMain(){
 	//Identify your threads here
 	pthread_t thread_led1, thread_led2;
 	
-	static unsigned int arg_led1 = 1;  //Index, must be declared static or global
-	static unsigned int arg_led2 = 0;  //Index, must be declared static or global
+	static unsigned int arg_led1[] = {0, 1};
+	static unsigned int arg_led2[] = {1, 2};
 
 	//Create your threads here
-	pthread_create(&thread_led1, NULL, tskFlashLED, &arg_led1);
-	pthread_create(&thread_led2, NULL, tskFlashLED, &arg_led2);
+	pthread_create(&thread_led1, NULL, tskFlashLED, arg_led1);
+	pthread_create(&thread_led2, NULL, tskFlashLED, arg_led2);
 }
---
+++ demo_posix/common/example/thread/led.c
@@ -0,0 +1,31 @@
+/************************************************************************************************
+ * File: 			app_led.c
+ * Description:		led task employing posix thread and file API
+ ***********************************************************************************************/
+
+#include <define.h>
+#include <unistd.h>
+
+/************************************************************************************************
+ * Function:            void* tskFlashLED(void* ptr)
+ * 
+ * Description:         toggle led at a specified rate
+ * 
+ * Input:               unsigned int[2]
+ *                      ptr[0]: led id
+ *                      ptr[1]: 50% duty cycle (in sec)
+ * 
+ * Output:              should never return
+ ***********************************************************************************************/
+#define ARG     ((unsigned int*)ptr)    
+void* tskFlashLED(void *ptr)
+{
+    start_process();
+
+    while(1){
+        usleep( 100000UL*ARG[1] );	
+        led_toggle(ARG[0]);
+    }
+
+    end_process();
+}
