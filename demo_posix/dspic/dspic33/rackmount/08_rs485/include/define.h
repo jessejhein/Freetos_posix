16811534d3ba4f212c9a9d6fc8198983da16132f
Dennis Tsang <dennis@amonics.com>
2008-01-24 02:48:11 +0000

    ==ADD== *drivers and rs485 examples for rackm

--- demo_posix/dspic/dspic33/rackmount/08_rs485/include/define.h
+++ demo_posix/dspic/dspic33/rackmount/08_rs485/include/define.h
@@ -18,10 +18,4 @@
 // Define User Idle Task Here **************************************************
 #define UserIdleTask()              idle_process(NULL)
 
-
-//  =============================================================================
-#define PWM_PRIMARY                 3   //Use OC4 (Channel 3)
-#define ADC_PRIMARY                 4   //Use AN4 
-#define DAC_PRIMARY                 DAC_CHB
-
 #endif /*DEFINE_H_*/
