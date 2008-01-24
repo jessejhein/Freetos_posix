bbde9f92d0bf987817d0296de46879ea68a469f0
Dennis Tsang <dennis@amonics.com>
2008-01-24 02:18:42 +0000

    ==ADD== *webserver example for benchtop

--- demo_posix/common/example/04_webserver/pf_main.c
+++ demo_posix/common/example/04_webserver/pf_main.c
@@ -35,10 +35,10 @@ void vSetupHardware( void ){
 void vUserMain(){
 	//Identify your threads here
 	pthread_t th_led1, th_http, th_laser_ctrl;
-	static unsigned int arg_led1 = 0; //Index, must be declared static or global
+	static unsigned int arg_led1[] = {0, 1};
 
 	//Create your threads here
-	pthread_create(&th_led1, NULL, tskFlashLED, &arg_led1);
+	pthread_create(&th_led1, NULL, tskFlashLED, arg_led1);
 	pthread_create(&th_http, NULL, tskHTTPServer, NULL);
     pthread_create(&th_laser_ctrl, NULL, tskLaserCtrl, NULL);
 }
