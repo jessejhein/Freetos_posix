99bb19e422af7d9f9c9c50c2ed1aa867f2f579f9
Dennis Tsang <dennis@amonics.com>
2008-05-19 03:53:21 +0000

    ==ADD== *documentation for time.h

--- posix/src/time.c
+++ posix/src/time.c
@@ -1,5 +1,7 @@
-/*
- * time.c
+/**
+ * \file
+ * Implementation for standard time.h
+ * \author Dennis Tsang <dennis@amonics.com>
  */
 
 #include <FreeRTOS.h>
@@ -8,72 +10,29 @@
 extern volatile time_t one_sec_cnt;
 #ifndef FREERTOS_SCHED 
     extern volatile time_t jiffies;
-#endif
+#endif /* NOT FREERTOS_SCHED */
 
-/**************************************************************************
- * Name:        time_t time(time_t *t)
- * 
- * Input:       If t is non-NULL, the return value is also stored in the memory 
- *              pointed to by t.
- * 
- * Output:      returns the time since the Epoch (time since last 
- *              reboot), measured in seconds.
- * 
- * CAUTION:     when using time() for comparing, do NOT compare like this:
- *                      while((int)time() <= wake_time) sleep(0); or
- *                      while(wake_time > (int)time()) sleep(0);
- *              this cause the program to go to infinite loop when
- *                      wake_time = max(int)
- *              to resolve
- *                      while((int)time() < wake_time) sleep(0); or
- *                      while(wake_time >= (int)time()) sleep(0);
- ***************************************************************************/
-time_t time(time_t *t)
+/*
+ * $one_sec_cnt is incremented by Timer Interrupt
+ */
+time_t 
+time(time_t *t)
 {
-    if(t != NULL)
-        *t = (time_t) one_sec_cnt;
-    return (time_t) one_sec_cnt;
+  if(t != NULL)
+    *t = (time_t) one_sec_cnt;
+  return (time_t) one_sec_cnt;
 }
 
-/**************************************************************************
- * Name:        time_t os_time(time_t *t)
- * 
- * Input:       If t is non-NULL, the return value is also stored in the memory 
- *              pointed to by t.
- * 
- * Output:      returns the time since the Epoch (time since last 
- *              reboot), measured in number of os clock ticks.
- * 
- * Note:        to compute the actual time (in ms) elapsed, multiply result by 
- *              portTICK_RATE_MS
- * 
- * CAUTION:     when using os_time() for comparing, do NOT compare like this:
- *                      while((int)os_time() <= wake_time) sleep(0); or
- *                      while(wake_time > (int)os_time()) sleep(0);
- *              this cause the program to go to infinite loop when
- *                      wake_time = max(int)
- *              to resolve
- *                      while((int)os_time() < wake_time) sleep(0); or
- *                      while(wake_time >= (int)os_time()) sleep(0);
- *************************************************************************** 
- * Maximum time tick is limited by the resolution of counter set by 
- * configUSE_16_BIT_TICKS and the frequency of context switch set by 
- * configTICK_RATE_HZ in <FreeRTOSConfig.h>
- * If configUSE_16_BIT_TICKS equals 1, the counter is 16-bit, otherwise the 
- * counter is 32-bit
- * For 16-bit counter with context switch period of 10ms, maximum number of 
- * sec is 65535*0.01 = 655 sec = 11 minutes
- * For 32-bit counter with context switch period of 10ms, maximum number of 
- * sec is 4,294,967,295*0.01 = 42,949,672 sec = 3.3 yrs
- ***************************************************************************/
-time_t os_time(time_t *t)
+//---------------------------------------------------------------------------
+time_t 
+os_time(time_t *t)
 {
 #ifdef FREERTOS_SCHED 
-    portTickType time = xTaskGetTickCount();
-#else
-    time_t time = jiffies;
-#endif        
-    if(t != NULL)
-        *t = (time_t) time;
-    return (time_t) time;
+  portTickType time = xTaskGetTickCount();
+#else /* NOT FREERTOS_SCHED */
+  time_t time = jiffies;
+#endif /* NOT FREERTOS_SCHED */
+  if(t != NULL)
+    *t = (time_t) time;
+  return (time_t) time;
 }
--- posix/time.h
+++ posix/time.h
@@ -1,6 +1,23 @@
-/*
- * time.h
+/**
+ * \addtogroup stdlib Standard Library
+ * @{
+ * 
+ * Implementation of Standard Library using FreeRTOS API
  */
+
+/**
+ * \defgroup time_h time.h
+ * @{
+ * 
+ * Implementation of Time function calls, interfacing between user applications and OS
+ */
+
+/**
+ * \file
+ * Implementation for standard time.h
+ * \author Dennis Tsang <dennis@amonics.com>
+ */
+
  
 #ifndef TIME_H_
 #define TIME_H_ 1
@@ -8,58 +25,51 @@
 #include <asm/types.h>
 #include <stddef.h>     //for definition of NULL
 
-/**************************************************************************
- * Name:        time_t time(time_t *t)
- * 
- * Input:       If t is non-NULL, the return value is also stored in the memory 
- *              pointed to by t.
- * 
- * Output:      returns the time since the Epoch (time since last 
- *              reboot), measured in seconds.
- * 
- * CAUTION:     when using time() for comparing, do NOT compare like this:
- *                      while((int)time() <= wake_time) sleep(0); or
- *                      while(wake_time > (int)time()) sleep(0);
- *              this cause the program to go to infinite loop when
- *                      wake_time = max(int)
- *              to resolve
- *                      while((int)time() < wake_time) sleep(0); or
- *                      while(wake_time >= (int)time()) sleep(0);
- ***************************************************************************/
- extern time_t time(time_t *t);
+/**
+ * \brief get the time (in seconds) since the Epoch (last reboot)
+ * \param t If t is non-NULL, the return value is also stored in the memory pointed to by t.
+ * \return the time since the Epoch
+ * \remarks When using time() for comparing, do NOT compare like this:
+ * \remarks \li while((int)time() <= wake_time) sleep(0); or
+ * \remarks \li while(wake_time > (int)time()) sleep(0);
+ * \remarks This cause the program to go to infinite loop when:
+ * \remarks \li wake_time = max(int)
+ * \remarks To resolve:
+ * \remarks \li while((int)time() < wake_time) sleep(0); or
+ * \remarks \li while(wake_time >= (int)time()) sleep(0);
+ **/
+extern time_t time(time_t *t);
 
-/**************************************************************************
- * Name:        time_t os_time(time_t *t)
+
+/**
+ * \brief get the time (in number of os clock ticks) since the Epoch (last reboot)
+ * \param t If t is non-NULL, the return value is also stored in the memory pointed to by t.
+ * \return the time since the Epoch
+ * \remarks To compute the actual time (in ms) elapsed, multiply result by portTICK_RATE_MS
  * 
- * Input:       If t is non-NULL, the return value is also stored in the memory 
- *              pointed to by t.
+ * \remarks When using os_time() for comparing, do NOT compare like this:
+ * \remarks \li while((int)os_time() <= wake_time) sleep(0); or
+ * \remarks \li while(wake_time > (int)os_time()) sleep(0);
+ * \remarks This cause the program to go to infinite loop when:
+ * \remarks \li wake_time = max(int)
+ * \remarks To resolve:
+ * \remarks \li while((int)os_time() < wake_time) sleep(0); or
+ * \remarks \li while(wake_time >= (int)os_time()) sleep(0);
  * 
- * Output:      returns the time since the Epoch (time since last 
- *              reboot), measured in number of os clock ticks.
- * 
- * Note:        to compute the actual time (in ms) elapsed, multiply result by 
- *              portTICK_RATE_MS
- * 
- * CAUTION:     when using os_time() for comparing, do NOT compare like this:
- *                      while((int)os_time() <= wake_time) sleep(0); or
- *                      while(wake_time > (int)os_time()) sleep(0);
- *              this cause the program to go to infinite loop when
- *                      wake_time = max(int)
- *              to resolve
- *                      while((int)os_time() < wake_time) sleep(0); or
- *                      while(wake_time >= (int)os_time()) sleep(0);
- *************************************************************************** 
- * Maximum time tick is limited by the resolution of counter set by 
- * configUSE_16_BIT_TICKS and the frequency of context switch set by 
- * configTICK_RATE_HZ in <FreeRTOSConfig.h>
- * If configUSE_16_BIT_TICKS equals 1, the counter is 16-bit, otherwise the 
- * counter is 32-bit
- * For 16-bit counter with context switch period of 10ms, maximum number of 
- * sec is 65535*0.01 = 655 sec = 11 minutes
- * For 32-bit counter with context switch period of 10ms, maximum number of 
- * sec is 4,294,967,295*0.01 = 42,949,672 sec = 3.3 yrs
- ***************************************************************************/
+ * \remarks Maximum time tick is limited by the resolution of counter set by
+ *          configUSE_16_BIT_TICKS and the frequency of context switch set by 
+ *          configTICK_RATE_HZ in <FreeRTOSConfig.h>
+ * \remarks If configUSE_16_BIT_TICKS equals 1, the counter is 16-bit, otherwise the 
+ *          counter is 32-bit
+ * \remarks For 16-bit counter with context switch period of 10ms, maximum number of 
+ *          sec is 65535*0.01 = 655 sec = 11 minutes
+ * \remarks For 32-bit counter with context switch period of 10ms, maximum number of 
+ *          sec is 4,294,967,295*0.01 = 42,949,672 sec = 3.3 yrs
+ **/
 extern time_t os_time(time_t *t);
 
 
 #endif /* TIME_H_ */
+
+/** @} */
+/** @} */
