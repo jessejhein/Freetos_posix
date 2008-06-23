700671745bcbc8e90f7836790dc4de749bd3b3c5
Dennis Tsang <dennis@amonics.com>
2008-06-23 07:51:56 +0000

    ==UPDATE== *documentation for ioctl

--- posix/example/unistd_ex.c
+++ posix/example/unistd_ex.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 
 int fd_uart;   //File descriptor for uart (RS232)
+int fd_adc;
 
 void* 
 tskComPort(void *ptr)
@@ -34,14 +35,23 @@ tskComPort(void *ptr)
     {
       //open com port for read and write
       fd_uart = open(UARTA, O_RDWR);
+      //open adc port for read only and non-blocking mode
+      fd_adc = open(ADC, O_RDONLY | O_NONBLOCK);
       
       while(1)
         {
           //read a character
           while(read(fd_uart, &uart_rx, 1) <= 0) usleep(0);
-          //write string
-          int number = sprintf(uart_tx, "%c%s%c", ++uart_rx, "@Amonics", 0x0d);
-          while(write(fd_uart, uart_tx, number) != number) usleep(0);
+
+          //read adc value from AN15
+          unsigned char ctrl_buf = 15;
+          ioctl(fd_adc, ADC_ADD_CH, &ctrl_buf);
+          int raw_adc;
+          while(read(fd_adc, &adc_raw, sizeof(adc_raw) ) != sizeof(adc_raw)) usleep(0);
+          
+          //write the adc value
+          int len = sprintf(uart_tx, "%d%c", ++uart_rx, adc_raw, 0x0d);
+          while(write(fd_uart, uart_tx, len) != len) usleep(0);
         }
     }
   end_process();
--- posix/sys/ioctl.h
+++ posix/sys/ioctl.h
@@ -1,36 +1,72 @@
-/************************************************************************************************
- * File: 			ioctl.h
- * Description:		definition of $request codes
- ***********************************************************************************************/
+/**
+ * \addtogroup posix POSIX
+ * @{
+ * 
+ * Implementation of POSIX API using FreeRTOS API
+ */
+
+/**
+ * \defgroup ioctrl IO Control for System Call
+ * @{
+ * 
+ * Definition of $request codes for System Calls
+ */
+
+/**
+ * \file
+ * IO Control
+ * \author Dennis Tsang <dennis@amonics.com>
+ */
 
 #ifndef _IOCTL_H_
 #define _IOCTL_H_
 
-#include <define.h>
-
+/*
+ * I2C Module
+ */
 #ifdef I2C_MOD
-//=============Request Code Definition: For io control=========================
-#define I2C_SET_STATUS			0			//set status bits
-#define I2C_START				0x01		//send start bit
-#define I2C_RESTART				0x02		//send restart bit
-#define I2C_STOP				0x04		//send stop bit
-#define I2C_NACK				0x08		//send not acknowledgment
- 
-//=============Request Code Definition: For io control=========================
-#define	 DAC_SET_CTL			0			//Select channel (A, B, C, or D)
-#endif //I2C_MOD
+/** set status bits */
+#define I2C_SET_STATUS      0
+/** send start bit */
+#define I2C_START           0x01
+/** send restart bit */
+#define I2C_RESTART         0x02
+/** send stop bit */
+#define I2C_STOP            0x04
+/** send not acknowledgment */
+#define I2C_NACK            0x08
 
+/** Select channel (A, B, C, or D) */
+#define DAC_SET_CTL         0
+#endif /* I2C_MOD */
+
+
+/*
+ * ADC Module
+ */
 #ifdef ADC_MOD
-//=============Request Code Definition: For io control=========================
-#define ADC_ADD_CH			0			//add a channel to current selection			
-#endif //ADC_MOD
+/** add a channel to current selection */
+#define ADC_ADD_CH          0
+#endif /* ADC_MOD */
 
+
+/*
+ * PWM Module
+ */
 #ifdef PWM_MOD
-//=============Request Code Definition: For io control=========================
-#define PWM_SELECT_CH		0			//select channel to use			
-#define PWM_SET_PERIOD		1			//set pwm period		
+/** select channel to use */
+#define PWM_SELECT_CH       0
+/** set pwm period */
+#define PWM_SET_PERIOD      1	
+#endif /* PWM_MOD */
 
-#endif //ADC_MOD
+
+/** 
+ * \example unistd_ex.c
+ * This is an example of how to use the system call module.
+ */
 
 #endif /*_IOCTL_H_*/
 
+/** @} */
+/** @} */
