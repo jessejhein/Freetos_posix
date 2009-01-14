3b7953bff94256582d144701bfebe023f5b22d27
Dennis Tsang <dennis@amonics.com>
2009-01-14 06:33:43 +0000

    ==REMOVE== *remove dulplicated trunk folder

--- trunk/posix/doc/readme.txt
+++
@@ -1,3 +0,0 @@
-==TO DO==
-*to generate documentation, under <posix/> execute:
-   doxygen
--- trunk/posix/fcntl.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FCNTL_H
-#define _FCNTL_H    1
-
-/* Get the definitions of O_*, F_*, FD_*: all the
-   numbers and flag bits for `open', `fcntl', et al.  */
-#include <bits/fcntl.h>
-
-#endif /* fcntl.h  */
