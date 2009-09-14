06eba58b5024344394f98f3eb672ae65c3a86bc7
Dennis Tsang <dennis@amonics.com>
2009-09-14 02:09:46 +0000

    ==ADD== *issue #72 *fft API

---
+++ dsp/fft.h
@@ -0,0 +1,68 @@
+/**
+ * \addtogroup dsp Digital Signal Processing Library
+ * @{
+ * 
+ * Implementation of DSP API
+ */
+
+/**
+ * \defgroup fft_lib Fast Fourier Transform
+ * @{
+ * 
+ * Implementation of Fast Fourier Transform
+ */
+
+/**
+ * \file
+ * Fast Fourier Transform
+ * \author Dennis Tsang <dennis@amonics.com>
+ */
+
+/**
+ * \page fft_page
+ * \section fft_sec1 INTRODUCTION
+ * \li This FFT library assume computation to be in-place to reduce RAM usage (i.e. original data are overwritten) 
+ * \section fft_sec2 COMPILE TIME CONFIGURATION
+ * \par Set the following before compiling
+ * \subsection fft_sec1_1 <.config_app>:
+ * \verbatim
+    CONFIG_ADC_SAMPLE_RATE            =   20480
+    CONFIG_ADC_ACQ_TIME               =   31
+    CONFIG_ADC_MAX_CH                 =   2
+    CONFIG_ADC_QSIZE                  =   512
+    CONFIG_ADC_OUTPUT_FORMAT          =   SIGNED_FRACTIONAL
+
+    CONFIG_FFT                        =   y
+    CONFIG_FFT_BLOCK_LENGTH           =   512
+    CONFIG_FFT_LOG2_BLK_LEN           =   9                     //= log2(FFT_BLOCK_LENGTH)
+   \endverbatim 
+ */
+
+#include <asm/types.h>
+
+/**
+ * \brief Assign sampled value to data point
+ * \param index data point id
+ * \param value value of sample data (adc raw data)
+ */
+extern void fft_set_data (int index, real_t value);
+
+
+/**
+ * \brief Perform a Fast Fourier Transform
+ * \return pointer to result
+ */
+extern complex_t* fft (void);
+
+
+/**
+ * \brief Get the square magnitude distribution from an FFT result
+ * \param data pointer to source (fft-ed) data
+ * \param index store the index correponding the maximum value
+ * \return pointer to result
+ */
+extern real_t* fft_sq_magnitude (complex_t* data, int* index);
+
+/** @} */
+/** @} */
+
--- posix/asm-dsPic/types.h
+++ posix/asm-dsPic/types.h
@@ -22,6 +22,7 @@
 #define TYPES_H_    1
 
 #include <FreeRTOS.h>
+#include <dsp.h>
 
 // ---------------------------------------------------------------------------------------
 /** unsigned 8-bit data */
@@ -54,15 +55,36 @@ typedef unsigned long           clock_t;
 
 
 // ---------------------------------------------------------------------------------------
+//Data type for complex number for <fft.h> : not POSIX
+/** complex numbers */
+typedef fractcomplex            complex_t;
+/** real numbers */
+typedef fractional              real_t;
+/** convert real data to floating point data */
+#define REAL2FLOAT(N)           Fract2Float(N)
+/** convert floating point data to real data */
+#define FLOAT2REAL(N)           Float2Fract(N)
+
+
+// ---------------------------------------------------------------------------------------
 /** Interrupt Request */
 #define _IRQ                    __attribute__((__interrupt__))
+
 /** TRAP IRQ for saving program counter: declare __u16 StkAddrLo, StkAddrHi in trap.c (order matters) */
 #define _TRAP_IRQ               __attribute__((__interrupt__(__preprologue__( \
                                   "mov #_StkAddrHi,w1\n\tpop [w1--]\n\tpop [w1++]\n\tpush [w1--]\n\tpush [w1++]"))))
+
 /** IO Stub Functions are placed in .libc section so that the standard libraries can access these functions using short jumps. */
 #define _LIBC                   __attribute__((section(".libc")))
+
 /** FAST RAM */
-#define _DMA                    __attribute__((space(dma),aligned(256)))
+#define _DMA                    __attribute__((space(dma), aligned(256)))
+
+/** Aligned Data in Program Memory */
+#define _PROG(N)                __attribute__ ((space(auto_psv), aligned(N)))
+
+/** Aligned Data in Y Memory */
+#define _YMEM(N)                __attribute__ ((section("data, ymemory"), aligned(N)))
 #endif /*TYPES_H_*/
 
 /** @} */
