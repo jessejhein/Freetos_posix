80dfd5444c5b5864b7c8d92c62f71e588ef48755
Dennis Tsang <dennis@amonics.com>
2011-05-18 02:01:54 +0000

    ==UPDATE== * move gettimeofday to <sys/time.h>

--- posix/sys/time.h
+++ posix/sys/time.h
@@ -37,42 +37,39 @@
  * along with freertos_posix.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#ifndef TIME_H_
-#define TIME_H_ 1
+#ifndef SYS_TIME_H_
+#define SYS_TIME_H_ 1
 
 #include <asm/types.h>
 #include <stddef.h>     //for definition of NULL
-#include <FreeRTOS.h>
 
 
-/** Number of clock ticks per second */
-#define CLOCKS_PER_SEC             configTICK_RATE_HZ
+/** A time value that is accurate to the nearest microsecond but also has a range of years. */
+struct timeval
+{
+  /** This represents the number of whole seconds of elapsed time. */
+  time_t tv_sec;        /* seconds */
+  /** This is the rest of the elapsed time (a fraction of a second), represented as the number of microseconds. It is always less than one million. */
+  suseconds_t tv_usec;  /* microseconds */
+};
+
+
+/** Structure crudely representing a timezone. */
+struct timezone
+{
+  int tz_minuteswest;   /* Minutes west of GMT.  */
+  int tz_dsttime;	/* Nonzero if DST is ever in effect.  */
+};
 
 
 /**
- * \brief get the time (in seconds) since the Epoch (last reboot)
- * \param t If t is non-NULL, the return value is also stored in the memory pointed to by t.
- * \return the time since the Epoch
+ * \brief get the current time, expressed as seconds and microseconds since the Epoch (last reboot)
+ * \param tv store time of day
+ * \param tz store timezone information, if tz is NULL, tz is not filled.
+ * \retval 0 success.
+ * \retval -1 errors.
  */
-extern time_t time (time_t *t);
-
-
-/**
- * \brief get the number of clock ticks since the Epoch (last reboot)
- * \return the time since the Epoch
- * \remarks To compute the actual time (in sec) elapsed, divide result by CLOCKS_PER_SEC
- *
- * \remarks Maximum time tick is limited by the resolution of counter set by
- *          configUSE_16_BIT_TICKS and the frequency of context switch set by 
- *          configTICK_RATE_HZ in <FreeRTOSConfig.h>
- * \remarks If configUSE_16_BIT_TICKS equals 1, the counter is 16-bit, otherwise the 
- *          counter is 32-bit
- * \remarks For 16-bit counter with context switch period of 10ms, maximum number of 
- *          sec is 65535*0.01 = 655 sec = 11 minutes
- * \remarks For 32-bit counter with context switch period of 10ms, maximum number of 
- *          sec is 4,294,967,295*0.01 = 42,949,672 sec = 3.3 yrs
- */
-extern clock_t clock (void);
+extern int gettimeofday (struct timeval *tv, struct timezone *tz);
 
 
 /** 
@@ -81,7 +78,7 @@ extern clock_t clock (void);
  */
 
 
-#endif /* TIME_H_ */
+#endif /* SYS_TIME_H_ */
 
 /** @} */
 /** @} */
