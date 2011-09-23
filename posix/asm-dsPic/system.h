71b0c8fe163e0be91e7134d963ca1427da9f1ae3
Dennis Tsang <dennis@amonics.com>
2011-09-23 02:49:23 +0000

    remove obsolete coding

--- posix/asm-dsPic/system.h
+++ posix/asm-dsPic/system.h
@@ -41,7 +41,7 @@
 #define SYSTEM_H_
 
 /* interrupt priority bits */
-#define IPL           ( 0x00e0 )
+#define IPL             ( 0x00e0 )
 
 /**
  * \brief CLear Interrupt/Disable Interrupt
@@ -49,7 +49,7 @@
  * Current priority level is stored in bit<7:5> of Status Register (SR). 
  * Setting Interrupt Priority Level (IPL) to 7 disables all interrupts (except traps).
  */
-#define cli()         SR |= IPL       //Set IPL to 7
+#define cli()           SR |= IPL       //Set IPL to 7
 
 
 /**
@@ -58,27 +58,7 @@
  * Current priority level is stored in bit<7:5> of Status Register (SR). 
  * Setting Interrupt Priority Level (IPL) to 7 disables all interrupts (except traps).
  */
-#define sti()         SR &= ~IPL      //Set IPL to 0
-
-
-/***************************************************************************************
- * DISI_PROTECT(X)
- * This MARCO does not belong to POSIX standard
- * This function is used to work around the silicon errata for dspic30 devices
- * When a low priority interrupt is negated by a high priority interrupt,
- * an AddressError will occur which will reset the target. 
- * To work around the problem, it is suggested by Microchip to protect  
- * 1) the clearing of Interrupt Flag
- * 2) the disabling of Interrupt Enable
- * 3) the lowering of Interrupt Priority 
- * 4) the modification of IPL in Status Register to 1-6
- ***************************************************************************************/
-#define DISI_PROTECT(X) \
-{ \
-  __asm__ volatile ("DISI #0x1FFF"); \
-  X; \
-  DISICNT = 0; \
-}
+#define sti()           SR &= ~IPL      //Set IPL to 0
 
 #endif /* SYSTEM_H_ */
 
